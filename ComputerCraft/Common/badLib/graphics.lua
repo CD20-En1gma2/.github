d00e199edb1ea4a1855ee797ba5f942b170c5b34
Andrea Atzei <71907618+LuciusHeit@users.noreply.github.com>
2022-05-23 23:31:24 +0000

    testing implementation of external draw functions

--- ComputerCraft/Common/badLib/graphics.lua
+++ ComputerCraft/Common/badLib/graphics.lua
@@ -1,11 +1,14 @@
 dofile("utils.lua")
 
+counter = 0
+
+-- Screen object, containing basic data about the screen
 Screen = {
     width = 1,
     height = 1
 }
 
-function Screen:new(obj)
+function Screen:new(obj) -- initializes the Screen object
     obj = obj or {}
     setmetatable(obj, self)
     self.__index = self
@@ -13,106 +16,124 @@ function Screen:new(obj)
     return obj
 end
 
-function Screen:reset()
+function Screen:reset() -- Resets the screen to its default state
     term.setBackgroundColor(colors.black)
     term.setTextColor(colors.white)
     term.setCursorPos(1, 1)
     term.clear()
 end
 
+
+-- Graphics object, most of the functions occur here
 Graphics = {
-    loop = false,
+    loop = false, -- boolean for the status of the loop function
     x = 0,
     y = 0,
-    t = "wooooooah",
-    screen = nil,
-    blink = false,
-    eventData = {""}
+    t = "wooooooah", -- temporary text variable
+    screen = nil, -- screen object
+    blink = false, -- temporary blink variable
+    eventData = {""}, -- collection of the events
+    printFunc = nil, -- list of functions to print in the loop
+    printFuncNum = nil, -- number of function to print in the loop
+    printArgs = nil -- parameters for the functions to print in the loop
 }
 
-function Graphics:new (obj)
+function Graphics:new (obj, func) -- initializes the Graphics object
     obj = obj or {}
     setmetatable(obj, self)
     self.__index = self
     self.loop = true
     self.screen = Screen:new()
+    if(func == nil or func[1] == nil)
+    then
+        self.printFunc = {}
+        self.printFunc[1] = Graphics:defaultPrint()
+    else
+        self.printFunc = func
+    end
+    self.printFuncNum = 1
+    self.printArgs = {Graphics}
     return obj
 end
 
-
-function Graphics:init()
+function Graphics:init() -- starts the Graphics
     Graphics:onStart()
-    parallel.waitForAny (self.stopProgram, self.onLoop)
+    Graphics:onLoop()
     Graphics:onFinish()
 end
 
-function Graphics:stop()
-    self.loop = false
+function Graphics:stop() -- stops the Graphics loop
+    Graphics.loop = false
 end
 
-function Graphics:onStart()
+function Graphics:onStart() -- function on startup of Graphics
     term.setBackgroundColor(colors.white)
     term.setTextColor(colors.purple)
 end
 
-function Graphics:onFinish()
+function Graphics:onFinish() -- function on closing of Graphics
     self.screen:reset()
     print("Finish")
 end
 
-function Graphics:onLoop()
+function Graphics:onLoop() -- main loop function
     while(Graphics.loop)
     do
-        Graphics:printBackground()
-        Graphics:middleText("test")
-        Graphics:middleText("WOOOOOOAH")
-        coroutine.yield()
+        parallel.waitForAny (self.stopProgram, self.draw)
     end
 end
 
+function Graphics:draw() -- main draw function
+    Graphics:printBackground() -- prints the background
+    term.setCursorPos(Graphics.screen.width+1, 0)
+    Graphics.printFunc[Graphics.printFuncNum](Graphics.printArgs) -- prints the content
+    print("test "..Graphics.printFuncNum)
+    coroutine.yield()
+end
+
 function Graphics:printBackground()
-    --[[w, h = term.getSize()
-    term.clear()
-    for i=0, w, 1
-    do
-        for j=0, h, 1
-        do
-            paintutils.drawPixel(i, j, colors.red)
-        end
-    end ]]--
     term.clear()
     term.setCursorPos(self.screen.width, 0)
-    if(self.blink)
-    then
-        local g = "false"
-        paintutils.drawBox(2, 2, 5, 5, colors.black)
-        term.setBackgroundColor(colors.white)
-        if(self.loop)
-        then 
-            g = "true"
-        end
-        print(g.." TEST "..term.getBackgroundColor())
-        term.setCursorPos(self.screen.width, 0)
-    end
-    self.blink = not (self.blink)
 end
 
 function Graphics:middleText(text)
     w, h = term.getSize()
-    s = (#(self.t))/2
+    s = (#(text))/2
     term.setCursorPos(w/2-s, h/2)
-    term.write(self.t)
+    term.write(text)
 end
 
 function Graphics:stopProgram()
-    while (Graphics.loop == true)
-    do
-        local tim = os.startTimer(0.2)
-        Graphics.activeEvents = {os.pullEvent()}
-        if ( hasValue(Graphics.activeEvents, "mouse_click"))
+    local tim = os.startTimer(0.05)
+    Graphics.activeEvents = {os.pullEvent()}
+    if ( hasValue(Graphics.activeEvents, "mouse_click"))
+    then
+        Graphics.t = Graphics.t.."jjj"
+        Graphics:stop()
+    end
+end
+
+function Graphics:setPrintArgs(args)
+    Graphics.printArgs = args
+end
+
+function Graphics:defaultPrint()
+    return function()
+        if(self.blink)
         then
-            Graphics.t = Graphics.t.."jjj"
-            Graphics.loop = false
+            local g = "false"
+            paintutils.drawBox(2, 2, 5, 5, colors.black)
+            term.setBackgroundColor(colors.white)
+            if(self.loop)
+            then 
+                g = "true"
+            end
+            print(g.." TEST "..term.getBackgroundColor())
+            term.setCursorPos(self.screen.width, 0)
         end
+        self.blink = not (self.blink)
+
+        Graphics:middleText("WOOOOOOAH "..counter)
+        counter = counter + 1
     end
 end
--- ComputerCraft/Common/badLib/main.lua
+++ ComputerCraft/Common/badLib/main.lua
@@ -1,4 +1,55 @@
 dofile("graphics.lua")
+--local vector = require "vector"
 
-local g = Graphics:new()
+-- Screen object, containing basic data about the screen
+Square = {
+    x = 1,
+    y = 1,
+    width = 1,
+    height = 1,
+    vec = nil
+}
+
+function Square:new(obj, x, y, w, h) -- initializes the Screen object
+    obj = obj or {}
+    setmetatable(obj, self)
+    self.__index = self
+    self.x = x
+    self.y = y
+    self.width = w
+    self.height = h
+    self.vec = vector:new(1, 0, 0)
+    return obj
+end
+
+function Square:bounce()
+    local w, h = term.getSize()
+    if(self.x + self.width == w or self.x == 0)
+    then
+        self.vec:mul(-1)
+    end
+end
+
+function Square:draw(sq)
+    print(sq.x)
+    paintutils.drawBox(sq.x, sq.y, sq.x+sq.width, sq.y+sq.height, colors.red)
+    term.setBackgroundColor(colors.white)
+end
+
+
+testSquareFuncInit = function (obj)
+    
+    Graphics:setPrintArgs({Square:new(nil, 3, 3, 3, 3)})
+    --obj.printFuncNum = obj.printFuncNum + 1
+    Graphics.printFuncNum = 2
+end
+testSquareFuncLoop = function (sq)
+    Square:draw(sq)
+    --Square:bounce()
+    sq.x = sq.x + sq.vec.x
+end
+
+
+
+local g = Graphics:new(nil, {testSquareFuncInit, testSquareFuncLoop})
 g:init()
