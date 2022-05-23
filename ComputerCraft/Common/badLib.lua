8c6b41918fb3059904362cb1c29d64bd243dd61c
Andrea Atzei <71907618+LuciusHeit@users.noreply.github.com>
2022-05-23 10:15:43 +0000

    Initialized badLib.lua

---
+++ ComputerCraft/Common/badLib.lua
@@ -0,0 +1,45 @@
+
+
+Graphics = {
+    loop = false,
+    x = 0,
+    y = 0
+}
+
+function Graphics:new (obj)
+    obj = obj or {}
+    setmetatable(obj, self)
+    self._index = self
+    self.loop = true
+    return obj
+end
+
+
+function Graphics:init()
+    Graphics:onStart()
+    while(self.loop)
+    do
+        Graphics:onLoop()
+    end
+    Graphics:onFinish()
+end
+
+function Graphics:stop()
+    self.loop = false
+end
+
+function Graphics:onStart()
+    return 
+end
+
+function Graphics:onLoop()
+    paintutils.drawPixel(Graphics.x, Graphics.y)
+    Graphics.x++
+    Graphics.y++
+end
+
+function Graphics:onFinish()
+    return
+end
+
+local g = Graphics:new(nil)
